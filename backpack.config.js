2ca9de366823bef8d5065bcfba64d8d71a7363bd
Juan Carvajal <juancarvajal.t119@gmail.com>
2020-05-25 02:19:39 -0500

    Implementacion de eslint y de typescript
    * Se configuro eslint para typescript
    
    * Se configuro typeScript para node
    
    * Se implemento el archivo tsconfig
    
    * Se mejoro el archivo backpack

--- backpack.config.js
+++ backpack.config.js
@@ -1,5 +1,20 @@
+/* eslint-disable no-param-reassign */
+const path = require('path')
+
 module.exports = {
   webpack: config => {
+    config.entry.main = './src/index.ts'
+    config.resolve.extensions.push('.ts')
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, 'src'),
+    }
+    config.module.rules.push(
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: [/node_modules/, path.resolve(__dirname, 'build')],
+      },
+    )
     return config
-  }
+  },
 }
